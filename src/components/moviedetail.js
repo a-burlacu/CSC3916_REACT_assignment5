79e9ae51d669c9a9c8fadd20ae912ec31f05d2d4
msalina007 <79892093+msalina007@users.noreply.github.com>
2023-04-26 18:37:50 -0600

    updated moviedetails

--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -1,67 +1,3 @@
-// import React, { Component } from 'react';
-// import { fetchMovie } from "../actions/movieActions";
-// import {connect} from 'react-redux';
-// import {Card, ListGroup, ListGroupItem } from 'react-bootstrap';
-// import { BsStarFill } from 'react-icons/bs'
-// import { Image } from 'react-bootstrap';
-//
-// class MovieDetail extends Component {
-//
-//     componentDidMount() {
-//         const {dispatch} = this.props;
-//         if (this.props.selectedMovie == null) {
-//             dispatch(fetchMovie(this.props.title));
-//         }
-//     }
-//
-//     render() {
-//         const DetailInfo = () => {
-//             if (!this.props.selectedMovie) {
-//                 return <div>Loading....</div>
-//             }
-//
-//             return (
-//                 <Card>
-//                     <Card.Header>Movie Detail</Card.Header>
-//                     <Card.Body>
-//                         <Image className="image" src={this.props.selectedMovie.imageURL} thumbnail />
-//                     </Card.Body>
-//                     <ListGroup>
-//                         <ListGroupItem>{this.props.selectedMovie.title}</ListGroupItem>
-//                         <ListGroupItem>
-//                             {this.props.selectedMovie.actors.map((actor, i) =>
-//                                 <p key={i}>
-//                                     <b>{actor.actorName}</b> {actor.characterName}
-//                                 </p>)}
-//                         </ListGroupItem>
-//                         <ListGroupItem><h4><BsStarFill/> {this.props.selectedMovie.avgRating}</h4></ListGroupItem>
-//                     </ListGroup>
-//                     <Card.Body>
-//                         {this.props.selectedMovie.reviews.map((review, i) =>
-//                             <p key={i}>
-//                                 <b>{review.username}</b>&nbsp; {review.review}
-//                                 &nbsp;  <BsStarFill /> {review.rating}
-//                             </p>
-//                         )}
-//                     </Card.Body>
-//                 </Card>
-//             )
-//         }
-//
-//         return (
-//             <DetailInfo />
-//         )
-//     }
-// }
-//
-// const mapStateToProps = state => {
-//     return {
-//         selectedMovie: state.movie.selectedMovie
-//     }
-// }
-//
-// export default connect(mapStateToProps)(MovieDetail);
-//
 import React, { Component } from 'react';
 import { fetchMovie } from "../actions/movieActions";
 import {connect} from 'react-redux';
@@ -74,7 +10,7 @@ class MovieDetail extends Component {
     componentDidMount() {
         const {dispatch} = this.props;
         if (this.props.selectedMovie == null) {
-            dispatch(fetchMovie(this.props.movieId));
+            dispatch(fetchMovie(this.props.title));
         }
     }
 
@@ -88,7 +24,7 @@ class MovieDetail extends Component {
                 <Card>
                     <Card.Header>Movie Detail</Card.Header>
                     <Card.Body>
-                        <Image className="image" src={this.props.selectedMovie.imageUrl} thumbnail />
+                        <Image className="image" src={this.props.selectedMovie.imageURL} thumbnail />
                     </Card.Body>
                     <ListGroup>
                         <ListGroupItem>{this.props.selectedMovie.title}</ListGroupItem>
@@ -107,10 +43,6 @@ class MovieDetail extends Component {
                                 &nbsp;  <BsStarFill /> {review.rating}
                             </p>
                         )}
-                        <form onSubmit={this.handleSubmit}>
-                            <input className='e-input' type='text' placeholder='Write a review' />
-                            <button type="submit">submit</button>
-                        </form>
                     </Card.Body>
                 </Card>
             )
@@ -129,3 +61,4 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps)(MovieDetail);
+
